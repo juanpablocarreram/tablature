d721bb463afd2415c01d1a522f2cf26065613b24
Juan Pablo Carrera <juanpablo.carrera.m@gmail.com>
2026-05-24 16:34:46 -0600

    Finish part 1

--- lib/tablature.ex
+++ lib/tablature.ex
@@ -1,8 +1,28 @@
 defmodule Tablature do
   def parse(tab) do
-    Regex.scan(~r/\d+/, tab)
-    |> List.flatten
-    |> Enum.map(fn n -> "B" <> n end)
-    |> Enum.join(" ")
+  lines = String.split(tab,["\n", "\r\n"], trim: true)
+  |> Enum.map(fn line ->
+    prefix = (Regex.run(~r/\D/, line) |> List.first())
+    # 2. Iterate through the graphemes of the line cleanly
+    line
+    |> String.slice(2..-2 // 1)
+    |> String.graphemes()
+    |> Enum.map(fn char ->
+      if char == "-" do
+        "x"
+      else
+        prefix <> char
+      end
+    end)
+  end)
+
+  Enum.zip_reduce(lines,"",fn elements,accumulator ->
+  string = Enum.filter(elements, fn element -> element != "x" end) |> Enum.join(" ")
+  cond do
+    accumulator == "" -> string
+    string == "" -> accumulator
+    true -> accumulator <> " " <> string
   end
+   end)
+end
 end
