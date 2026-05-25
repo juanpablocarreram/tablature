e050e0a31414210398a356d73da7e05aa9bf1f48
Juan Pablo Carrera <juanpablo.carrera.m@gmail.com>
2026-05-25 11:48:29 -0600

    Finish part 5

--- lib/tablature.ex
+++ lib/tablature.ex
@@ -1,35 +1,59 @@
 defmodule Tablature do
   def parse(tab) do
-  lines = String.split(tab,["\n", "\r\n"], trim: true)
-  |> Enum.map(fn line ->
-    prefix = (Regex.run(~r/\D/, line) |> List.first())
-    # 2. Iterate through the graphemes of the line cleanly
-    line
-    |> String.slice(2..-2 // 1)
-    |> String.graphemes()
-    |> Enum.map(fn char ->
+    lines = tab |> String.trim() |> String.split(["\n", "\r\n"], trim: false)
+    [lastGroup, segmentLines, _counter] = Enum.reduce(lines,[[],[],0], fn line, [actualTablature,listOfLists,counter] ->
       cond do
-        char == "-" -> "x"
-        char == "|" -> "o"
-        true -> prefix <> char
+        counter == 6 -> [[],listOfLists ++ [actualTablature],0]
+        true -> [actualTablature ++ [line],listOfLists,counter + 1]
       end
     end)
-  end)
+    segmentedLines = (segmentLines ++ [lastGroup])
+    |> Enum.map(fn segment -> segment |> Enum.map(fn line ->
+      prefix = (Regex.run(~r/\D/, line) |> List.first())
+      # 2. Iterate through the graphemes of the line cleanly
+      line
+      |> String.slice(2..-2 // 1)
+      |> String.graphemes()
+      |> Enum.map(fn char ->
+        cond do
+          char == "-" -> "x"
+          char == "|" -> "o"
+          true -> prefix <> char
+        end
+      end)
+    end) end)
 
-  Enum.zip_reduce(lines,["",0],fn elements, [accumulatorString, acummulatorBlanks] ->
-  string = Enum.filter(elements, fn element -> element != "x" end) |> Enum.join("/")
-  cond do
-    string == "o/o/o/o/o/o" -> [accumulatorString, acummulatorBlanks]
-    acummulatorBlanks == 3 -> [accumulatorString <> " " <> "_ " <> string,0]
-    accumulatorString == "" ->
+    Enum.reduce(segmentedLines, "", fn segment, acc ->
+    segmentResult = Enum.zip_reduce(segment,["",0],fn elements, [accumulatorString, acummulatorBlanks] ->
+    string = Enum.filter(elements, fn element -> element != "x" end) |> Enum.join("/")
+    newAcc =
     if string == "" do
-      [accumulatorString, acummulatorBlanks + 1]
+      acummulatorBlanks + 1
     else
-      [string, acummulatorBlanks]
+      0
     end
-    string == "" -> [accumulatorString, acummulatorBlanks + 1]
-    true -> [accumulatorString <> " " <> string,0]
+    cond do
+      #Si el string actual es una linea de separacion dentro de un mismo segmento
+      string == "o/o/o/o/o/o" -> [accumulatorString, acummulatorBlanks]
+      #Si el acumulador tiene 3 espacios seguidos
+      newAcc == 3 ->
+        [accumulatorString <> " _",0]
+
+      accumulatorString == "" ->
+        [string,0]
+      #Si el string actual no tiene ninguna nota
+      string == "" -> [accumulatorString, newAcc]
+      #Si el string actual tiene alguna nota
+      true -> [accumulatorString <> " " <> string,0]
+
+    end
+    end) |> List.first()
+    if acc == "" do
+      segmentResult
+    else
+      acc <> " " <> segmentResult
+    end
+    end)
+
   end
-   end) |> List.first()
-end
 end
